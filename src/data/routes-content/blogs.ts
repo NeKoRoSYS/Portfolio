c1c82254b3d8f3a40afd8e19c653ae11f822597b
NeKoRoSYS <nekorosys@gmail.com>
2026-09-07 19:57:12 +0800

    type fixes

--- src/data/routes-content/blogs.ts
+++ src/data/routes-content/blogs.ts
@@ -1,10 +1,11 @@
+import { StaticImageData } from "next/image";
 import { announcementPosts } from "./blogs/announcements";
 
 export interface BlogMeta {
   slug: string;
   title: string;
   excerpt: string;
-  thumbnail?: string;
+  thumbnail?: string | StaticImageData;
   date: string;
   file: string;
   tags: string[];
