6d87a79f870a9809fc6ea73a22392434f7131c49
NeKoRoSYS <nekorosys@gmail.com>
2026-09-08 17:59:53 +0800

    hero polish

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,40 @@
 import type { ReactNode } from "react";
-import Block from "./Block";
 import { cn } from "@/lib/utils";
 
 interface HeroProps {
+  id?: string;
   className?: string;
   children?: ReactNode;
   background?: ReactNode;
+  borderVisible?: boolean;
   fade?: boolean;
 }
 
 export default function Hero({
   className,
+  id,
+  borderVisible,
   children,
   background,
   fade,
 }: HeroProps) {
   return (
-    <Block className={cn("relative min-h-svh bg-[rgba(19,13,28,1)]")}>
+    <section
+      id={id}
+      className={cn(
+        "relative flex min-h-svh w-full flex-col items-center justify-center overflow-hidden bg-[rgba(19,13,28,1)]",
+        borderVisible && "border-b border-zinc-800",
+      )}
+    >
       <div className="pointer-events-none absolute inset-0 z-0">
         {background}
       </div>
+
       {fade && (
         <div className="pointer-events-none absolute inset-0 z-0 bg-linear-to-b from-black/0 to-black" />
       )}
 
-      <div className="pointer-events-none relative z-10 flex min-h-full w-full flex-col justify-center py-24">
+      <div className="relative z-10 flex w-full max-w-6xl flex-col justify-center px-8 py-24">
         <div
           className={cn(
             "pointer-events-auto w-full drop-shadow-lg drop-shadow-black/50",
@@ -34,6 +44,6 @@ export default function Hero({
           {children}
         </div>
       </div>
-    </Block>
+    </section>
   );
 }
