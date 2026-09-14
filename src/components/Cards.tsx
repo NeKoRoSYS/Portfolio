aeb60a8b99659285c76e08564f9817b0dd410c67
NeKoRoSYS <nekorosys@gmail.com>
2026-09-14 23:57:23 +0800

    adjustment

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -173,9 +173,9 @@ export function ProjectCard({
               >
                 <TextHyperlink
                   showHyperlinkIcon
-                  linkIconClass="w-5 group-touch-hover:bg-green-400 bg-zinc-400"
+                  linkIconClass="w-5 group-touch-hover:bg-zinc-100 bg-zinc-400"
                   className={cn(
-                    "font-cosmic text-zinc-100 transition-transform sm:touch-hover:translate-x-2 group-touch-hover:text-green-400",
+                    "font-cosmic text-zinc-100 transition-transform touch-hover:text-purple-300! sm:touch-hover:translate-x-2 group-touch-hover:text-green-400",
                     featured ? "text-3xl" : "text-xl sm:text-2xl",
                   )}
                   path={projectLink}
