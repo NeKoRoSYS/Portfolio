dd4578f3a4323e6104974889c4ddd9d041180126
NeKoRoSYS <nekorosys@gmail.com>
2026-09-15 17:58:48 +0800

    fix

--- src/data/routes-content/home/home.tsx
+++ src/data/routes-content/home/home.tsx
@@ -235,7 +235,7 @@ export const SECTIONS: SectionProps[] = [
                 name={link.altName}
                 icon={link.icon}
                 className={cn(
-                  "relative z-10 h-16 w-full rounded-xl border px-4 font-bold lg:justify-start",
+                  "relative z-10 h-16 w-full rounded-xl border px-4 font-bold backdrop-blur-xs lg:justify-start",
                   index > 0 && "self-end lg:w-3xs touch-hover:text-zinc-100",
                   link.colors,
                 )}
